33c78abf668609d399ea6a497a72aeb5385e2419
Sam <samuelsergi@outlook.com>
2018-06-20 17:51:09 +1000

    Add Clicky Information

--- uHub/src/main/java/com/frozenorb/uhub/listeners/ClickListeners.java
+++ uHub/src/main/java/com/frozenorb/uhub/listeners/ClickListeners.java
@@ -1,5 +1,7 @@
 package com.frozenorb.uhub.listeners;
 
+import com.frozenorb.uhub.spawn.joinitems.JoinItems;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,5 +12,16 @@ public class ClickListeners implements Listener {
     @EventHandler
     public void onClick(PlayerInteractEvent event){
         Player player = event.getPlayer();
+
+        if (player.getItemInHand() == null || player.getItemInHand().getType() == Material.AIR){
+            return;
+        }
+
+        for (JoinItems joinItems : JoinItems.values()){
+            if (joinItems.getHubItem().getItem().isSimilar(player.getItemInHand())){
+                joinItems.getHubItem().getClickHandler().click(player);
+                return;
+            }
+        }
     }
 }
