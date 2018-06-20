0b9f4381b07980bc23e5cbe35917e9e040fbf67f
Sam <samuelsergi@outlook.com>
2018-06-20 14:55:34 +1000

    Ping Listener

---
+++ Cheatbreaker-Only-Proxy/src/main/java/com/frozenorb/cbonlyproxy/listeners/ProxyPingListener.java
@@ -0,0 +1,21 @@
+package com.frozenorb.cbonlyproxy.listeners;
+
+import com.frozenorb.cbonlyproxy.CBProxyPlugin;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ProxyPingListener implements Listener {
+
+    @EventHandler
+    public void onServerConnect(ServerConnectEvent e) {
+        ProxiedPlayer player = e.getPlayer();
+        if (player.getPendingConnection().getVersion() != -1332){
+            Logger.getAnonymousLogger().log(Level.SEVERE, "LOL");
+        }
+    }
+}
