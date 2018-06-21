a5756c56401747448036c326b79f2e503534228d
Sam <samuelsergi@outlook.com>
2018-06-22 07:47:23 +1000

    Finished up the Ping Command for players to view their own ping

--- Basic/src/main/java/com/frozenorb/basic/commands/PingCommand.java
+++ Basic/src/main/java/com/frozenorb/basic/commands/PingCommand.java
@@ -1,10 +1,13 @@
 package com.frozenorb.basic.commands;
 
 import com.frozenorb.commonlibs.utils.MessageUtility;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class PingCommand implements CommandExecutor {
 
@@ -15,7 +18,35 @@ public class PingCommand implements CommandExecutor {
             return false;
         }
         Player player = (Player) commandSender;
-        player.sendMessage(MessageUtility.formatMessage("&f" + player.getDisplayName() + "&e's Ping: &c0"));
+        player.sendMessage(MessageUtility.formatMessage("&f" + player.getDisplayName() + "&e's Ping: &c" + pingPlayer(player)));
         return false;
+    }
+
+    /**
+     * Neat little class to get a player's ping without using NMS
+     *
+     * @see "https://bukkit.org/threads/get-players-ping.337634/"
+     */
+    public int pingPlayer(Player who) {
+        try {
+            //Building the version of the server in such a form we can use it in NMS code.
+            String bukkitversion = Bukkit.getServer().getClass().getPackage()
+                    .getName().substring(23);
+            //Getting craftplayer to cast later on
+            Class<?> craftPlayer = Class.forName("org.bukkit.craftbukkit."
+                    + bukkitversion + ".entity.CraftPlayer");
+            //Invoking method getHandle() for the craftplayer we build above, and now we have a EntityPlayer
+            Object handle = craftPlayer.getMethod("getHandle").invoke(who);
+            //Getting field "ping" that holds player's ping obviously
+            Integer ping = (Integer) handle.getClass().getDeclaredField("ping").get(handle);
+            //Returning the ping
+            return ping.intValue();
+        } catch (ClassNotFoundException | IllegalAccessException
+                | IllegalArgumentException | InvocationTargetException
+                | NoSuchMethodException | SecurityException
+                | NoSuchFieldException e) {
+            //Handle exceptions however you like, i chose to return value of -1; since player's ping can't be -1.
+            return -1;
+        }
     }
 }
