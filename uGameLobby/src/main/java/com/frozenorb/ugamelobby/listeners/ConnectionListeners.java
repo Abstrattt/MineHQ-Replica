69ab80dddc8286976bed5ce8081ed19a7ea5df62
Sam <samuelsergi@outlook.com>
2018-06-23 17:29:19 +1000

    Remove player from GameQueue on leave

--- uGameLobby/src/main/java/com/frozenorb/ugamelobby/listeners/ConnectionListeners.java
+++ uGameLobby/src/main/java/com/frozenorb/ugamelobby/listeners/ConnectionListeners.java
@@ -1,5 +1,8 @@
 package com.frozenorb.ugamelobby.listeners;
 
+import com.frozenorb.ugamelobby.queue.ModeQueue;
+import com.frozenorb.ugamelobby.queue.QueueHandler;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,9 +11,21 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class ConnectionListeners implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {}
+    public void onJoin(PlayerJoinEvent event) {
+        /* Player Object */
+        Player player = event.getPlayer();
+    }
 
     @EventHandler
-    public void onQuit(PlayerQuitEvent event) {}
+    public void onQuit(PlayerQuitEvent event) {
+        /* Player Object */
+        Player player = event.getPlayer();
+
+        /* Check if the player is queued */
+        if (QueueHandler.isPlayerQueued(player)){
+            ModeQueue queue = QueueHandler.getQueue(player);
+            queue.getQueued().remove(player.getUniqueId());
+        }
+    }
 
 }
