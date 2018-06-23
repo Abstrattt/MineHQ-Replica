7c0f7d63d25f7a2b4650dcecc7ed485fded4cc60
Sam <samuelsergi@outlook.com>
2018-06-23 17:29:38 +1000

    Added Required Players for Game Queue

--- uGameLobby/src/main/java/com/frozenorb/ugamelobby/queue/ModeQueue.java
+++ uGameLobby/src/main/java/com/frozenorb/ugamelobby/queue/ModeQueue.java
@@ -1,16 +1,18 @@
 package com.frozenorb.ugamelobby.queue;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Getter
+@Getter @Setter
 public class ModeQueue {
 
     private List<UUID> queued = new ArrayList<>();
     private String id;
+    private int requiredPlayers;
 
     public ModeQueue(String id) {
         this.id = id;
