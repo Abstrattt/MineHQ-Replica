9547b821c51959e6f263c11b2496618e40f9721e
Sam <samuelsergi@outlook.com>
2018-06-28 16:06:04 +1000

    Added comments

--- qModSuite/src/main/java/com/frozenorb/qmodsuite/profile/StaffProfileHandler.java
+++ qModSuite/src/main/java/com/frozenorb/qmodsuite/profile/StaffProfileHandler.java
@@ -8,16 +8,26 @@ import java.util.Map;
 
 public class StaffProfileHandler {
 
+    /* Staff Profiles Collection */
     @Getter private static Map<PlayerProfile, StaffProfile> profiles = new HashMap<>();
 
+    /**
+     * Add a profile to the collection
+     */
     public static void addProfile(PlayerProfile playerProfile, StaffProfile staffProfile) {
         profiles.put(playerProfile, staffProfile);
     }
 
+    /**
+     * Remove a profile from the collection
+     */
     public static void removeProfile(PlayerProfile playerProfile) {
         profiles.remove(playerProfile);
     }
 
+    /**
+     * Get a profile
+     */
     public static StaffProfile getProfile(PlayerProfile playerProfile){
         return profiles.get(playerProfile);
     }
